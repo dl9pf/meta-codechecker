dl9pf/meta-codechecker

commit 16e35b165a758ef047f9caa31b9d4b44987ffc11

chodechecker: update to 6.25.1
Bump to the latest codechecker version. Tested:

- report HTML
- report TXT
- report CODECLIMATE

Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

classes/codechecker.bbclass
@@ -82,7 +82,7 @@ if test x"${CODECHECKER_ENABLED}" = x"1"; then
     export CC_LOGGER_FILE="${B}/compile_commands.json"
     export CC_ANALYSE_OUT="${DEPLOY_DIR}/CodeChecker/${PN}/results/"
     if test -f ${CC_LOGGER_FILE} ; then
-        CodeChecker analyze ${PARALLEL_MAKE} ${CODECHECKER_ANALYZE_EXTRA_ARGS} -o ${CC_ANALYSE_OUT} --report-hash context-free-v2 ${CC_LOGGER_FILE} || true
+        CodeChecker analyze ${PARALLEL_MAKE} --analyzers clang-tidy ${CODECHECKER_ANALYZE_EXTRA_ARGS} -o ${CC_ANALYSE_OUT} --report-hash context-free-v2 ${CC_LOGGER_FILE} || true
     fi
 fi
 }

recipes-devtools/codechecker/codechecker_6.17.0.bb
@@ -1,54 +1,0 @@
-SUMMARY = "CodeChecker static analysis tool"
-HOMEPAGE = "https://codechecker.readthedocs.io/en/latest/"
-LICENSE = "Apache-2.0"
-
-# The pypi package doesn't include a license file
-LIC_FILES_CHKSUM = "file://LICENSE.TXT;md5=2e982d844baa4df1c80de75470e0c5cb"
-
-SRC_URI[md5sum] = "edd6a862af82a573f924ef14b40dd4db"
-SRC_URI[sha256sum] = "1affc5fc6b21749050e36530868583c672852a448fd07bc7a221eb1d7d05b663"
-
-PYPI_PACKAGE = "codechecker"
-
-inherit pypi setuptools3-base
-
-SRC_URI += " file://0001-Don-t-require-fix-version-of-dependancies.patch"
-
-DEPENDS += "\
-    ${PYTHON_PN}-pip-native \
-"
-
-RDEPENDS_${PN} += "python3 python3-modules"
-
-# Requirements from web/requirements.txt
-RDEPENDS_${PN} += "python3-lxml python3-sqlalchemy python3-alembic python3-portalocker python3-psutil python3-mypy-extensions"
-
-# Requirements from analyzers/requirements.txt
-RDEPENDS_${PN} += "python3-pyyaml python3-git"
-
-RDEPENDS_${PN}_class-native += " clang-native"
-RDEPENDS_${PN}_class-nativesdk += " nativesdk-clang"
-
-do_install() {
-    # CodeChecker use a native namespace package and can't be installed using
-    # setup.py install, so we can't simply ihnerit from setuptools3
-    # Instead, we must install using pip install .
-    # This code is inspired by the distutils3.bbclass
-    install -d ${D}${PYTHON_SITEPACKAGES_DIR}
-
-
-    PYTHONPATH=${D}${PYTHON_SITEPACKAGES_DIR} \
-    ${STAGING_BINDIR_NATIVE}/${PYTHON_PN}-native/${PYTHON_PN} -m pip install . \
-      --no-index --no-deps \
-      --prefix=${prefix} \
-      --root=${D}
-
-    for i in ${D}${bindir}/* ${D}${sbindir}/*; do
-        if [ -f "$i" ]; then
-            sed -i -e s:${PYTHON}:${USRBINPATH}/env\ python3:g $i
-            sed -i -e s:${STAGING_BINDIR_NATIVE}:${bindir}:g $i
-        fi
-    done
-}
-
-BBCLASSEXTEND += "native nativesdk"

recipes-devtools/codechecker/codechecker_6.25.1.bb
@@ -1,0 +1,39 @@
+SUMMARY = "CodeChecker static analysis tool"
+HOMEPAGE = "https://codechecker.readthedocs.io/en/latest/"
+LICENSE = "Apache-2.0"
+
+# The pypi package doesn't include a license file
+LIC_FILES_CHKSUM = "file://LICENSE.TXT;md5=2e982d844baa4df1c80de75470e0c5cb"
+
+SRC_URI[sha256sum] = "65f619bd3274e4466c0ce8ae475e5b428344aec5f970c5b5ffb025801012512c"
+
+PYPI_PACKAGE = "codechecker"
+
+inherit pypi setuptools3
+
+DEPENDS += "\
+    ${PYTHON_PN}-pip-native \
+"
+
+do_patch:append() {
+    bb.build.exec_func('do_relax_version_requirements', d)
+}
+
+do_relax_version_requirements () {
+    sed -i -e "s/==/>=/g" ${S}/analyzer/requirements.txt
+    sed -i -e "s/==/>=/g" ${S}/web/requirements.txt
+    sed -i -e "s/==/>=/g" ${S}/build_dist/CodeChecker/lib/python3/codechecker.egg-info/requires.txt
+}
+
+RDEPENDS:${PN} += "python3 python3-modules"
+
+# Requirements from web/requirements.txt
+RDEPENDS:${PN} += "python3-lxml python3-sqlalchemy python3-alembic python3-portalocker python3-psutil python3-multiprocess python3-mypy-extensions python3-thrift sarif-tools"
+
+# Requirements from analyzers/requirements.txt
+RDEPENDS:${PN} += "python3-pyyaml python3-git"
+
+RDEPENDS_${PN}:class-native += " clang-native"
+RDEPENDS_${PN}:class-nativesdk += " nativesdk-clang"
+
+BBCLASSEXTEND += "native nativesdk"

recipes-devtools/codechecker/files/0001-Don-t-require-fix-version-of-dependancies.patch
@@ -1,76 +1,0 @@
-From ed1f0a985c8c0b81cabcedc9b93a8731c2544744 Mon Sep 17 00:00:00 2001
-From: Samuel Dolt <samuel.dolt.ext@siemens.com>
-Date: Thu, 16 Sep 2021 17:44:01 +0200
-Subject: [PATCH] Don't require fix version of dependancies
-
----
- analyzer/requirements.txt                      | 10 +++++-----
- .../python3/codechecker.egg-info/requires.txt  | 18 +++++++++---------
- web/requirements.txt                           | 16 ++++++++--------
- 3 files changed, 22 insertions(+), 22 deletions(-)
-
-diff --git a/analyzer/requirements.txt b/analyzer/requirements.txt
-index de5d76c..37de81b 100644
---- a/analyzer/requirements.txt
-+++ b/analyzer/requirements.txt
-@@ -1,5 +1,5 @@
--lxml==4.6.3
--portalocker==2.2.1
--psutil==5.8.0
--PyYAML==5.4.1
--mypy_extensions==0.4.3
-+lxml
-+portalocker
-+psutil
-+PyYAML
-+mypy_extensions
-diff --git a/build_dist/CodeChecker/lib/python3/codechecker.egg-info/requires.txt b/build_dist/CodeChecker/lib/python3/codechecker.egg-info/requires.txt
-index 0bceb22..94c0a46 100644
---- a/build_dist/CodeChecker/lib/python3/codechecker.egg-info/requires.txt
-+++ b/build_dist/CodeChecker/lib/python3/codechecker.egg-info/requires.txt
-@@ -1,9 +1,9 @@
--PyYAML==5.4.1
--gitpython==3.1.11
--mypy_extensions==0.4.3
--sqlalchemy==1.3.23
--portalocker==2.2.1
--alembic==1.5.5
--lxml==4.6.3
--thrift==0.13.0
--psutil==5.8.0
-+PyYAML
-+gitpython
-+mypy_extensions
-+sqlalchemy
-+portalocker
-+alembic
-+lxml
-+thrift
-+psutil
-diff --git a/web/requirements.txt b/web/requirements.txt
-index 961ec6c..24633ea 100644
---- a/web/requirements.txt
-+++ b/web/requirements.txt
-@@ -1,11 +1,11 @@
--lxml==4.6.3
--sqlalchemy==1.3.23
--alembic==1.5.5
--portalocker==2.2.1
--psutil==5.8.0
--mypy_extensions==0.4.3
--thrift==0.13.0
--gitpython==3.1.11
-+lxml
-+sqlalchemy
-+alembic
-+portalocker
-+psutil
-+mypy_extensions
-+thrift
-+gitpython
- 
- ./api/py/codechecker_api/dist/codechecker_api.tar.gz
- ./api/py/codechecker_api_shared/dist/codechecker_api_shared.tar.gz
--- 
-2.20.1
-

recipes-devtools/python/python3-multiprocess_0.70.15.bb
@@ -1,0 +1,14 @@
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://LICENSE;md5=b9e5c5521f21a2e6301f5ce9591f5f18"
+
+SRC_URI[sha256sum] = "e814d8249f6751f174d012c240b02601b877b78e5c4fe5dd00b30dc796f01ee4"
+
+SRC_URI = "git://github.com/uqfoundation/multiprocess;protocol=https;branch=master"
+
+SRCREV = "2255a2837e08f30f4ab07b3cb73f0c7ed2973d5f"
+
+S = "${WORKDIR}/git"
+
+inherit setuptools3
+
+BBCLASSEXTEND = "native nativesdk"

recipes-devtools/sarif-tools/sarif-tools_1.0.0.bb
@@ -1,0 +1,16 @@
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://LICENSE;md5=d4a904ca135bb7bc912156fee12726f0"
+
+SRC_URI[sha256sum] = "e814d8249f6751f174d012c240b02601b877b78e5c4fe5dd00b30dc796f01ee4"
+
+SRC_URI = "git://github.com/microsoft/sarif-tools;protocol=https;branch=main"
+
+SRCREV = "c76ebfed17565d7197011430b14fba13232d6108"
+
+S = "${WORKDIR}/git"
+
+RDEPENDS:${PN} += "python3-jinja2"
+
+inherit python_poetry_core
+
+BBCLASSEXTEND = "native nativesdk"

commit 5e34b719317fd77a8a09439c16a5a333ed2b312b

classes/codechecker.bbclass: Fix LD_LIBRARY path and depends
For simple scenario where we don't use meson. cmake and make
we should use LD_PRELOAD to generate the compile json file. This
fix a regression with the new code-checker

Fixes: 16e35b165a75 ("chodechecker: update to 6.25.1")
Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

classes/codechecker.bbclass
@@ -26,7 +26,7 @@ python () {
             if bb.data.inherits_class('meson', d):
                 codechecker_use_compile_commands_json_from_configure = True
 
-            codechecker_deps = ' codechecker-native:do_populate_sysroot python3-six-native:do_populate_sysroot python3-thrift-native:do_populate_sysroot clang-native:do_populate_sysroot python3-native:do_populate_sysroot python3-psutil-native:do_populate_sysroot python3-portalocker-native:do_populate_sysroot python3-pyyaml-native:do_populate_sysroot python3-git-native:do_populate_sysroot python3-alembic-native:do_populate_sysroot python3-sqlalchemy-native:do_populate_sysroot python3-mypy-extensions-native:do_populate_sysroot python3-lxml-native:do_populate_sysroot python3-markupsafe-native:do_populate_sysroot'
+            codechecker_deps = ' codechecker-native:do_populate_sysroot python3-six-native:do_populate_sysroot python3-thrift-native:do_populate_sysroot clang-native:do_populate_sysroot python3-native:do_populate_sysroot python3-psutil-native:do_populate_sysroot python3-portalocker-native:do_populate_sysroot python3-pyyaml-native:do_populate_sysroot python3-git-native:do_populate_sysroot python3-alembic-native:do_populate_sysroot python3-sqlalchemy-native:do_populate_sysroot python3-mypy-extensions-native:do_populate_sysroot python3-lxml-native:do_populate_sysroot python3-markupsafe-native:do_populate_sysroot python3-multiprocess-native:do_populate_sysroot sarif-tools:do_populate_sysroot'
             codecheckeranalyse_after = None
 
             if codechecker_use_compile_commands_json_from_configure:
@@ -49,7 +49,7 @@ SAVEDENV = ""
 
 python do_csprecompile () {
     SAVEDENV = os.environ.copy()
-    os.environ["LD_PRELOAD"] = "" + d.getVar('RECIPE_SYSROOT_NATIVE') + "/usr/lib/python" + d.getVar('PYTHON_BASEVERSION') +"/site-packages/codechecker_analyzer/ld_logger/lib/ldlogger.so"
+    os.environ["LD_PRELOAD"] = "" + d.getVar('RECIPE_SYSROOT_NATIVE') + "/usr/lib/python" + d.getVar('PYTHON_BASEVERSION') +"/site-packages/codechecker_analyzer/ld_logger/lib/" + d.getVar('SDK_ARCH') + "/ldlogger.so"
     os.environ["CC_LOGGER_GCC_LIKE"] = "gcc:g++:clang:clang++:cc:c++:ccache"
     os.environ["CC_LOGGER_FILE"] = "" + d.getVar("B") + "/compile_commands.json"
     #os.environ["PARALLEL_MAKE"] = "" + d.getVar("PARALLEL_MAKE")